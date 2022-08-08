cb41475b824e8c859d36a21d27cd810276c029f2
megasoby <megasoby@ssg.com>
2022-08-08 14:21:27 +0900

    settleBatchProc first commit

---
+++ src/main/java/io/megasoby/settlebatchproc/SettleBatchProcApplication.java
@@ -0,0 +1,13 @@
+package io.megasoby.settlebatchproc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleBatchProcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettleBatchProcApplication.class, args);
+    }
+
+}
